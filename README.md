cbcf2be5aaa6a1bf44b70018ed69027cfd0d0b75
lucianlavric <[lucianlavric2005@gmail.com]>
2024-08-19 10:39:30 -0400

    Added drawing functionality to grid when mouse hovering

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch-odin
+
+A dynamic etch a sketch application created using javascript and html
--- script.js
+++ script.js
@@ -9,3 +9,17 @@ for(let n=0; n<16; n++){
         container.appendChild(div);
     }
 }
+
+
+//Select the div via querySelector
+document.querySelectorAll(".div").forEach(item => {
+    //detects when the cursor hovers over a div
+    item.addEventListener("mouseover", event =>{
+        //Changes the div to black
+        item.setAttribute("style", "background-color:black");
+    });
+    
+});
+
+
+
